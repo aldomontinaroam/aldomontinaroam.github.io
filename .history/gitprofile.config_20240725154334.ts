4dfc56367f2febbdc333da33bca7b4b8430cec88
aldomontinaroam <aldomontinaro.am@gmail.com>
2024-07-25 15:51:09 +0200

    Prepare for deployment

---
+++ .history/gitprofile.config_20240725154334.ts
@@ -0,0 +1,245 @@
+// gitprofile.config.ts
+
+const CONFIG = {
+  github: {
+    username: 'aldomontinaroam', // Your GitHub org/user name. (This is the only required config)
+  },
+  /**
+   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
+   * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
+   * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
+   */
+  base: '/',
+  projects: {
+    github: {
+      display: true, // Display GitHub projects?
+      header: 'GitHub Projects',
+      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      automatic: {
+        sortBy: 'updated', // Sort projects by 'stars' or 'updated'
+        limit: 8, // How many projects to display.
+        exclude: {
+          forks: false, // Forked projects will not be displayed if set to true.
+          projects: [
+            'aldomontinaroam/aldomontinaroam.github.io', 
+            'aldomontinaroam/bestr_unisi_badges.github.io',
+            'aldomontinaroam/dm2_files',
+            'aldomontinaroam/xai24_dataset5',
+          ], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        },
+      },
+      manual: {
+        // Properties for manually specifying projects
+        //projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+      },
+    },
+    external: {
+      header: 'Other Projects',
+      // To hide the `External Projects` section, keep it empty.
+      projects: [
+        /* {
+          title: 'Project Name',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+          imageUrl:
+            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+          link: 'https://example.com',
+        },
+        {
+          title: 'Project Name',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+          imageUrl:
+            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+          link: 'https://example.com',
+        }, */
+      ],
+    },
+  },
+  seo: {
+    title: 'Aldo Montinaro',
+    description: '',
+    imageURL: '',
+  },
+  social: {
+    linkedin: 'aldomontinaro',
+    //twitter: '',
+    researchGate: '',
+    facebook: '',
+    instagram: 'aldo.montinaro',
+    reddit: '',
+    threads: 'aldo.montinaro',
+    youtube: '', // example: 'pewdiepie'
+    udemy: '',
+    dribbble: '',
+    behance: '',
+    //medium: 'arifszn',
+    //dev: 'arifszn',
+    stackoverflow: '', // example: '1/jeff-atwood'
+    skype: '',
+    telegram: '',
+    //website: 'https://www.arifszn.com',
+    phone: '3663415890',
+    email: 'aldomontinaro.am@gmail.com',
+  },
+  resume: {
+    fileUrl:
+      '', // Empty fileUrl will hide the `Download Resume` button. 
+  },
+  skills: [
+    'Python',
+    'SQL',
+    'R',
+    'MS Office',
+    'Data Mining',
+    'Machine Learning'
+  ],
+  experiences: [
+    {
+      company: 'SoftJam S.p.A.',
+      position: 'Associate Developer',
+      from: 'Jun 2023',
+      to: 'Oct 2023',
+      companyLink: 'https://www.softjam.it',
+    },
+    {
+      company: 'DIESSE Diagnostica Senese S.p.A.',
+      position: 'Research and Development Intern',
+      from: 'Aug 2022',
+      to: 'Oct 2022',
+      companyLink: 'https://www.diesse.it',
+    },
+    {
+      company: 'Università degli Studi di Siena (Giovanisì)',
+      position: 'Front Office Assistant',
+      from: 'Oct 2021',
+      to: 'Jun 2022',
+      companyLink: 'https://orientarsi.unisi.it/studio/struttura/biblioteca-di-area-scientifico-tecnologica-complesso-san-niccolo',
+    },
+  ],
+  /* certifications: [
+    {
+      name: 'Lorem ipsum',
+      body: 'Lorem ipsum dolor sit amet',
+      year: 'March 2022',
+      link: 'https://example.com',
+    },
+  ], */
+  educations: [
+    {
+      institution: 'Università di Pisa',
+      degree: 'MSc, Data Science and Business Informatics',
+      from: '2023',
+      to: 'Present',
+    },
+    {
+      institution: 'Università degli Studi di Siena',
+      degree: 'BSc, Physics and Advanced Technologies',
+      from: '2019',
+      to: '2023',
+    },
+  ],
+  /* publications: [
+    {
+      title: 'Publication Title',
+      conferenceName: '',
+      journalName: 'Journal Name',
+      authors: 'John Doe, Jane Smith',
+      link: 'https://example.com',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      title: 'Publication Title',
+      conferenceName: 'Conference Name',
+      journalName: '',
+      authors: 'John Doe, Jane Smith',
+      link: 'https://example.com',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+  ], */
+  // Display articles from your medium or dev account. (Optional)
+  blog: {
+    source: 'dev', // medium | dev
+    username: '', // to hide blog section, keep it empty
+    limit: 2, // How many articles to display. Max is 10.
+  },
+  googleAnalytics: {
+    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  },
+  // Track visitor interaction and behavior. https://www.hotjar.com
+  hotjar: {
+    id: '',
+    snippetVersion: 6,
+  },
+  themeConfig: {
+    defaultTheme: 'emerald',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
+    disableSwitch: true,
+
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
+    respectPrefersColorScheme: false,
+
+    // Display the ring in Profile picture
+    displayAvatarRing: true,
+
+    // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'dim',
+      'nord',
+      'sunset',
+      'procyon',
+    ],
+
+    // Custom theme, applied to `procyon` theme
+    customTheme: {
+      primary: '#fc055b',
+      secondary: '#219aaf',
+      accent: '#e8d03a',
+      neutral: '#2A2730',
+      'base-100': '#E3E3ED',
+      '--rounded-box': '3rem',
+      '--rounded-btn': '3rem',
+    },
+  },
+
+  // Optional Footer. Supports plain text or HTML.
+  footer: `&copy; Copyright 2024 Aldo Montinaro`,
+
+  enablePWA: true,
+};
+
+export default CONFIG;
